0b6e5acd49698bb3da31d754b69b238a2cacf301
MickeyJ <mickeyjmalotte@gmail.com>
2025-05-08 17:46:38 -0700

    init commit

---
+++ packages/marketing/src/bootstrap.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App';
+
+// Mount function
+const mount = (el) => {
+    ReactDOM.render(
+        <App/>,
+        el
+    )
+}
+
+// Call mount In dev and in isolation
+if (process.env.NODE_ENV === 'development'){
+    const devRoot = document.querySelector('#_marketing-dev-root')
+    devRoot && mount(devRoot)
+}
+
+// Export mount function
+export { mount }
